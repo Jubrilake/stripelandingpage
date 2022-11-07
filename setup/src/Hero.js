6fb033d768c15bfbac638b8cd4e1097eed7ebb9f
bayo <jubrilake@gmail.com>
2022-11-07 13:25:39 +0100

    first commit

---
+++ setup/src/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import phoneImg from './images/phone.svg'
+import { useGlobalContext } from './context'
+
+const Hero = () => {
+  const {closeSubmenu} = useGlobalContext()
+  return <section className='hero' onMouseOver={closeSubmenu}>
+    <div className="hero-center">
+      <article className='hero-info'>
+        <h1>Payments infrastructure for the internet</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque doloribus aliquam mollitia alias, hic voluptatem. Dolores deleniti quasi tenetur ut fuga repudiandae, exercitationem facere ea rerum, reiciendis earum fugiat magni.</p>
+        <button className='btn'>Start now</button>
+
+      </article>
+      <article className='hero-images'>
+        <img src={phoneImg} className="phone-img" alt="phone" />
+      </article>
+    </div>
+  </section>
+}
+
+export default Hero
